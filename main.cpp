fe93cf912f5d4aa3915e416adfa75f3a08dfbc10
puqugithub <827607510@qq.com>
2016-01-16 18:32:15 +0800

    完善main中调用所有函数

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <string.h>
 using namespace CPPCOMMON;
 
+void PrintVec( vector<string> vec );
+
 int main()
 {
 	cout << "\n#===========Demo===========#\n" << endl;
@@ -12,6 +14,7 @@ int main()
 	string str = "nanjing";
 	cout << "string_format()的结果：" << string_format( "This is [%s].\n", str.c_str() ) << endl;
 	cout << endl;
+
 
 	//StripStr	删除首部和尾部的'\n'和'\t'	string StripStr(const string& str, const string& patternstr = " \n\t");
 	str = "nanjing    ";
@@ -31,6 +34,65 @@ int main()
 	cout << endl;
 	
 
+	//Split		划分字符串	vector<string> SplitStr(const string& source, const string& pattern)
+	vec.clear();
+	str = "nanjing\tyangzhou\tshanghai";
+	vec = SplitStr( str, "\t" );
+	//vec = SplitStr( str );  //默认为" ", "\t", "\n"其中一个，而且只能默认一个
+	cout << "SplitStr()的结果为:" << endl;
+	PrintVec( vec );
+	cout << endl;
+
+
+	//UpperStr	字母大写	string UpperStr(const string& strIn)
+	str = "nanjing";
+	cout << "UpperStr()之后的结果为：" << UpperStr( str ) << endl;
+	cout << endl;
+
+
+	//LowerStr	字母小写	string LowerStr(const string& strIn)
+	str = "NanJing";
+	cout << "LowerStr()之后的结果为：" << LowerStr( str ) << endl;
+	cout << endl;
+
+
+	//ReplaceStr	替换前几个字符串	string ReplaceStr(const string& strSrc, const string& oldStr, const string& newStr, int count)
+	str = "beijingbeijingnanjingbeijing";
+	string oldStr = "bei";
+	string newStr = "nan";
+	int count = 2;
+	res = ReplaceStr( str, oldStr,newStr, 2 );  //只替换前2个"bei"
+	cout << "ReplaceStr替换str之前：" << str << endl;
+	cout << "replaceStr替换str之后：" << res << endl;
+	cout << endl;
+
+
+	//CountStrDistance	计算两个字符串间的距离		unsigned int CountStrDistance(const string& A, const string& B)
+	string strA = "beijing";
+	string strB = "nanjing";
+	string strC = "yangzhou";
+	cout << "CountStrDistance( nanjing => beijing ): " <<CountStrDistance( strA, strB ) << endl;
+	cout << "CountStrDistance( nanjing => 'yangzhou ): " <<CountStrDistance( strC, strB ) << endl;
+	cout << endl;
+
+
+	//CountStrSimilarity		计算两个字符串间的相似个数	unsigned int CountStrSimilarity(const string& A, const string& B)
+	cout << "CountStrSimilarity( nanjing => beijing ): " <<CountStrSimilarity( strA, strB ) << endl;
+	cout << "CountStrSimialrity( nanjing => 'yangzhou ): " <<CountStrSimilarity( strC, strB ) << endl;
+	cout << endl;
+
 	
 	return 0;
+}
+
+
+
+
+void PrintVec( vector<string> vec )
+{
+	vector< string >::iterator iter = vec.begin();
+	for( ; iter != vec.end(); iter++ )
+	{
+		cout << *iter << endl;
+	}
 }
--- str_functs.h
+++ str_functs.h
@@ -1,6 +1,5 @@
 /************************************
  * file enc : utf8
- * author   : wuyanyi09@gmail.com
 ************************************/
 #ifndef CPPCOMMON_STR_FUNCTS_H
 #define CPPCOMMON_STR_FUNCTS_H
@@ -25,50 +24,20 @@ namespace CPPCOMMON
 
 	string JoinStr(const vector<string>& source, const string& connector);
 
-	vector<string> splitStr(const string& source, const string& pattern = " \t\n");
-	void splitStr(const string& source, vector<string>& out_vec, const string& pattern = " \t\n");
-	bool splitStrMultiPatterns(
-				const string& strSrc, 
-				vector<string>& outVec, 
-				const vector<string>& patterns
-				);
-	string upperStr(const string& str);
-	string lowerStr(const string& str);
-	string replaceStr(const string& strSrc, const string& oldStr, const string& newStr, int count = -1);
+	vector<string> SplitStr(const string& source, const string& pattern = " \t\n");
+
+	void SplitStr(const string& source, vector<string>& out_vec, const string& pattern = " \t\n");
+
+	string UpperStr(const string& str);
+
+	string LowerStr(const string& str);
+
+	string ReplaceStr(const string& strSrc, const string& oldStr, const string& newStr, int count = -1);
 	string StripStr(const string& str, const string& patternstr = " \n\t");
-	unsigned int countStrDistance(const string& A, const string& B);
-	unsigned int countStrSimilarity(const string& A, const string& B);
 
-    //encode
-    //size_t unicodeToUtf8(uint16_t *in, size_t len, char * out);
-	//string unicodeToUtf8(const Unicode& unicode);
-    //int utf8ToUnicode(const char* inutf8, int len, uint16_t* unicode);
-	//bool utf8ToUnicode(const string& utfStr, Unicode& unicode);
+	unsigned int CountStrDistance(const string& A, const string& B);
 
-	//int code_convert(const char *from_charset,const char *to_charset,char *inbuf,size_t inlen,char *outbuf,size_t outlen);
-	//string gbkToUtf8(const string& gbk);
-	//string utf8ToGbk(const string& utf);
-
-	bool uniStrToVec(const string& str, Unicode& vec);
-	string uniVecToStr(const Unicode& vec);
-
-	inline uint16_t twocharToUint16(char high, char low)
-	{
-		return (((uint16_t(high) & 0x00ff ) << 8) | (uint16_t(low) & 0x00ff));
-	}
-
-	inline pair<char, char> uint16ToChar2(uint16_t in)
-	{
-		pair<char, char> res;
-		res.first = (in>>8) & 0x00ff; //high
-		res.second = (in) & 0x00ff; //low
-		return res;
-	}
-
-	inline void printUnicode(const Unicode& unicode)
-	{
-		cout<<uniVecToStr(unicode)<<endl;
-	}
+	unsigned int CountStrSimilarity(const string& A, const string& B);
 
 	
 }
